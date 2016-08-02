1a12790c229e707ac69fb687ea6dea132add6821
Kayla Andersen <kaynandersen@gmail.com>
2016-08-02 12:33:00 -0600

    Started dataquery folder to hold cypher queries

--- dataquery/get_inner_circle.py
+++ dataquery/get_inner_circle.py
@@ -1,9 +1,13 @@
+import cypher
 
 
-
-
-cypherq = """MATCH (n:User)<-[r:FOLLOWS]-(m:User)
+cypherq = """MATCH (u1:User)<-[r:FOLLOWS]-(u2:User)
                              WHERE m.screen_name = 'BernieSanders'
                              AND NOT EXISTS(n.following_added)
                              RETURN n.id
                              LIMIT 100;"""
+
+bernie_circle = """MATCH p=(m:User)-[r:FOLLOWS]->(n:User)
+                   WHERE m.screen_name = 'BernieSanders'
+                   AND n.following_added = True
+                   RETURN p LIMIT 100;"""
