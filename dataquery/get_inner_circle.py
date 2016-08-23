53b1d2e75f4e4e7cc74da048973e16085ec997ab
Kayla Andersen <kaynandersen@gmail.com>
2016-08-22 22:08:19 -0600

    Updated my readme and folder structure

--- dataquery/get_inner_circle.py
+++ dataquery/get_inner_circle.py
@@ -1,13 +1,34 @@
 import cypher
 
+conn = "http://neo4j:IYWcb1ar2@ec2-54-197-153-60.compute-1.amazonaws.com:7474"
 
-cypherq = """MATCH (u1:User)<-[r:FOLLOWS]-(u2:User)
-                             WHERE m.screen_name = 'BernieSanders'
-                             AND NOT EXISTS(n.following_added)
-                             RETURN n.id
-                             LIMIT 100;"""
 
-bernie_circle = """MATCH p=(m:User)-[r:FOLLOWS]->(n:User)
-                   WHERE m.screen_name = 'BernieSanders'
-                   AND n.following_added = True
-                   RETURN p LIMIT 100;"""
+query = """MATCH p = (u1:User)<-[r:FOLLOWS]->(u2:User)-->
+           WHERE u1.screen_name = 'BernieSanders'
+           AND EXISTS(u2.following_added)
+           RETURN relationships(p);"""
+#
+query_test = """MATCH p=()-[r:FOLLOWS]->()
+                RETURN p LIMIT 50;"""
+
+query_test = """MATCH
+                p=(u1:User{screen_name:"BernieSanders"})-[r:FOLLOWS]->(u2:User)
+                WHERE u1.screen_name = 'BernieSanders'
+                AND EXISTS(u2.following_added)
+                RETURN u2.screen_name, u3.screen_name;"""
+
+query_path = """MATCH       p=(n:User{screen_name:'BernieSanders'})-[r1:FOLLOWS]->(m:User)-[r2:FOLLOWS]-(o:User)<-[r3:FOLLOWS]-(n:User{screen_name:'BernieSanders'}) RETURN p;"""
+
+quey_test = """MATCH """
+
+query_bernie = """MATCH (user)"""
+
+query3 = """MATCH (c:User{screen_name : 'BernieSanders'})-[r:*0..1]-(d:User)
+            WITH c, collect(r) as rs
+            RETURN c, rs"""
+
+result = cypher.run(query_test, conn=conn)
+ig = IGraph.TupleList(result)
+
+# closest betweeness
+between = [(node["name"], node.betweenness()) for node in ig.vs]
